99ec4074ffc0a08e1ee9bb16ee2c1b18dd007706
Maira Beatriz de Almeida Lacerda <mairaallacerda@gmail.com>
2024-08-15 17:02:15 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Página inicial onde o usuário escolhe o tipo de cálculo
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Rota para a página de escolha do sólido
+@app.route('/choose_solid')
+def choose_solid():
+    return render_template('choose_solid.html')
+ 
+# Rotas para cada página de cálculo de sólido
+@app.route('/paralelepipedo')
+def paralelepipedo():
+    return render_template('paralelepipedo.html')
+
+@app.route('/cubo')
+def cubo():
+    return render_template('cubo.html')
+
+@app.route('/circulo')
+def circulo():
+    return render_template('circulo.html')
+
+@app.route('/circulo_vazado')
+def circulo_vazado():
+    return render_template('circulo_vazado.html')
+
+# Rota para a página da Lei de Hooke
+@app.route('/lei_de_hooke')
+def lei_de_hooke():
+    return render_template('lei_de_hooke.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
